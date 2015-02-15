043e306c24ae166e3b9bcb72012562a28239a719
kuchta300 <kuchta.300@icloud.com>
2015-02-15 15:11:38 -0500

    Mid Level Save
    This is an attempt to save state before I change more stuff

--- Taskit/ViewController.swift
+++ Taskit/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //UITableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        println(indexPath.row)
+        
+        var cell: TaskCell = tableView.dequeueReusableCellWithIdentifier("myCell") as TaskCell
+        
+        cell.taskLabel.text = "Study Anticlastic Raising"
+        
+        cell.descriptionLabel.text = "Torcs from the past"
+        
+        cell.dateLabel.text = "02/15/2015"
+        
+        return cell
+    
+    }
+    
+    //UITableViewDelegate
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
 }
 
