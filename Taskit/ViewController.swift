3f70580fc96af198b0153b39cc0461bcb950a716
kuchta300 <kuchta.300@icloud.com>
2015-02-16 10:16:41 -0500

    Dictionary Model
    All changes made to support the dictionary model of the task display.

--- Taskit/ViewController.swift
+++ Taskit/ViewController.swift
@@ -11,11 +11,18 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+   
+    var taskArray:[Dictionary<String,String>] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let task1:Dictionary<String, String> = ["task": "Study French", "subtask": "Verbs", "date": "01/14/2014"]
+        let task2:Dictionary<String, String> = ["task": "Eat Dinner", "subtask": "Burgers", "date": "01/14/2014"]
+        let task3:Dictionary<String, String> = ["task": "Gym", "subtask": "Leg day", "date": "01/14/2014"]
+        taskArray = [task1, task2, task3]
         
+        self.tableView.reloadData()
         
     }
 
@@ -26,21 +33,25 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     //UITableViewDataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return taskArray.count
+        
     }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         println(indexPath.row)
         
+        let taskDict:Dictionary = taskArray[indexPath.row]
         var cell: TaskCell = tableView.dequeueReusableCellWithIdentifier("myCell") as TaskCell
         
-        cell.taskLabel.text = "Study Anticlastic Raising"
+        cell.taskLabel.text = taskDict["task"]
         
-        cell.descriptionLabel.text = "Torcs from the past"
+        cell.descriptionLabel.text = taskDict["subtask"]
         
-        cell.dateLabel.text = "02/15/2015"
+        cell.dateLabel.text = taskDict["date"]
+    
         
         return cell
+        
     
     }
     
